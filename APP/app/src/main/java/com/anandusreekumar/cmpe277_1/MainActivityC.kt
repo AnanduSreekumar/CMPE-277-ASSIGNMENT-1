4e86b1c8b7a8f68ef33c494f0f5ee13c6f379c92
Anandu Sreekumar <anandu.sreekumar@outlook.com>
2024-03-10 23:16:12 -0700

    assignment 1 code

---
+++ APP/app/src/main/java/com/anandusreekumar/cmpe277_1/MainActivityC.kt
@@ -0,0 +1,58 @@
+package com.anandusreekumar.cmpe277_1
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.ExperimentalUnitApi
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
+import androidx.compose.ui.unit.dp
+import com.anandusreekumar.cmpe277_1.ui.theme.CMPE277_1Theme
+
+@ExperimentalUnitApi
+class MainActivityC : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            CMPE277_1Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column(
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Column(
+                            modifier = Modifier.padding(20.dp),
+                            verticalArrangement = Arrangement.spacedBy(10.dp, Alignment.CenterVertically),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                        ) {
+                            Text("Activity C", fontSize = TextUnit(20f, TextUnitType.Sp))
+                            Text("Activity C occupies the complete window real-estate. The Android OS forces Activity A to be Paused. However, the background thread keep incrementing the counter")
+                        }
+                    }
+                }
+            }
+        }
+
+        val resultIntent = Intent()
+        resultIntent.data = Uri.parse("10")
+
+        setResult(RESULT_OK, resultIntent)
+    }
+}
